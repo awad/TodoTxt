f639a05ec61b98f13c9ec6f365b9a64e85f1f5fe
Amit Wadhwa <awadhwa@live.com>
2014-02-11 20:19:03 -0800

    First commit

---
+++ TodoTxt/Model/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoTxt.Model
+{
+    public class Task
+    {
+        private string Priority { get; set; }
+        private bool IsDone { get; set; }
+        public string Description { get; set; }
+    }
+}
